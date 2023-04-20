c502a715353de3d78c1fc8a011b90b27ef9d5fac
Gerardo Miranda <gerardodev25@gmail.com>
2023-04-20 19:50:00 -0400

    009 Crear items - Servicio y DTOs

---
+++ src/items/dto/inputs/create-item.input.ts
@@ -0,0 +1,26 @@
+import { InputType, Field, Float } from '@nestjs/graphql';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
+@InputType()
+export class CreateItemInput {
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @Field(() => Float)
+  @IsNumber()
+  @IsPositive()
+  quantity: number;
+
+  @Field(() => String, { nullable: true })
+  @IsString()
+  @IsOptional()
+  quantityUnits?: string;
+}
