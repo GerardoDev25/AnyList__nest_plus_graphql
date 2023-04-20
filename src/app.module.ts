a3cfa1eae67ecbd40cdef017e815b373e673b5b0
Gerardo Miranda <gerardodev25@gmail.com>
2023-04-19 21:10:56 -0400

    007 Conectar base de datos a Nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { join } from 'path';
-import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
