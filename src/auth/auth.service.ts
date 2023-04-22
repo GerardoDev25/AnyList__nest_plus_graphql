0c26aa30e8cfb03517ceedd1e2ac144e3b1e6922
Gerardo Miranda <gerardodev25@gmail.com>
2023-04-21 21:04:18 -0400

    013 Passport Module y JWT Module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,7 +4,6 @@ import * as bcrypt from 'bcrypt';
 import { LoginInput, SingnUpInput } from './dto/inputs';
 import { AuthResponse } from './types/auth-response.type';
 import { UsersService } from 'src/users/users.service';
-import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
