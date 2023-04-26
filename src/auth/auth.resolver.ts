f4deef04b104675ee8caa38f58afd71a416f91a0
Gerardo Miranda <gerardodev25@gmail.com>
2023-04-25 20:24:47 -0400

    020 Autorización de usuarios - Roles

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,4 @@
-import { UnauthorizedException, UseGuards } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 import { AuthService } from './auth.service';
@@ -7,6 +7,7 @@ import { AuthResponse } from './types/auth-response.type';
 import { JwtAuthGuard } from './guards/jwt.guard';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from 'src/users/entities/user.entity';
+// import { ValidRoles } from './enums/valid-roles.enums';
 
 @Resolver()
 export class AuthResolver {
@@ -28,8 +29,9 @@ export class AuthResolver {
 
   @Query(() => AuthResponse, { name: 'revalidate' })
   @UseGuards(JwtAuthGuard)
-  revalidateToken(@CurrentUser() user: User): AuthResponse {
-    console.log('revaldate token', user);
+  revalidateToken(
+    @CurrentUser(/*[ValidRoles.admin]*/) user: User,
+  ): AuthResponse {
     return this.authService.revalidateToken(user);
   }
 }
--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -2,19 +2,35 @@ import {
   ExecutionContext,
   InternalServerErrorException,
   createParamDecorator,
+  ForbiddenException,
 } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { ValidRoles } from '../enums/valid-roles.enums';
+import { User } from 'src/users/entities/user.entity';
 
 export const CurrentUser = createParamDecorator(
-  (roles = [], context: ExecutionContext) => {
+  (roles: ValidRoles[] = [], context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
-    const user = ctx.getContext().req.user;
+    const user = ctx.getContext().req.user as User;
 
+    console.log(roles);
     if (!user) {
       throw new InternalServerErrorException(
         'no user inside the request - make sure that we used the AuthGuard',
       );
     }
-    return user;
+
+    if (roles.length === 0) return user;
+
+    for (const role of user.roles) {
+      // TODO eliminar valid role
+      if (roles.includes(role as ValidRoles)) {
+        return user;
+      }
+    }
+
+    throw new ForbiddenException(
+      `user: ${user.fullName} need a valid role : ${roles}`,
+    );
   },
 );
