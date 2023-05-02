4ba1c4a8b44b647a93bd851a52a65544cf05f550
Gerardo Miranda <gerardodev25@gmail.com>
2023-05-02 19:47:14 -0400

    008 Seed - Paso 4 Crear items

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,23 +1,26 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+
 import { Item } from 'src/items/entities/item.entity';
 import { User } from 'src/users/entities/user.entity';
-import { Repository } from 'typeorm';
-import { SEED_USERS } from './data/data-seed';
+import { SEED_ITEMS, SEED_USERS } from './data/data-seed';
 import { UsersService } from 'src/users/users.service';
+import { ItemsService } from 'src/items/items.service';
 
 @Injectable()
 export class SeedService {
   private isProd: boolean;
 
   constructor(
-    private readonly configService: ConfigService,
+    configService: ConfigService,
     @InjectRepository(Item)
     private readonly itemRepository: Repository<Item>,
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly userService: UsersService,
+    private readonly itemService: ItemsService,
   ) {
     this.isProd = configService.get('STATE') === 'prod';
   }
@@ -31,10 +34,9 @@ export class SeedService {
 
     // ! crear usuarios
     const user = await this.loadUser();
-    console.log(user);
 
     // ! crear items
-
+    await this.loadItems(user);
     return true;
   }
 
@@ -52,5 +54,16 @@ export class SeedService {
     }
 
     return users[0];
+  }
+
+  async loadItems(user: User): Promise<boolean> {
+    const items = [];
+    for (const item of SEED_ITEMS) {
+      items.push(this.itemService.create(item, user));
+    }
+
+    await Promise.allSettled(items);
+
+    return true;
   }
 }
